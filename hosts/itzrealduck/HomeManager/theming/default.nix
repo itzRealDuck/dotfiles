f6420e99f00ed4bb3154d882863aed2e5ce6b4c1
itzRealDuck <nermeenbasha555@gmail.com>
2024-08-01 00:29:13 +0300

    fix gtk, and add a few other shit to gtk

--- hosts/itzrealduck/HomeManager/theming/default.nix
+++ hosts/itzrealduck/HomeManager/theming/default.nix
@@ -2,6 +2,7 @@
   option = {
     qt.enable = true;
     gtk.enable = true;
+    gtk.thname = "Nordic-bluish-accent-standard-buttons";
     catppuccin.pointerCursor.enable = true;
   };
 }
--- modules/HomeConfig/theming/gtk/default.nix
+++ modules/HomeConfig/theming/gtk/default.nix
@@ -5,7 +5,7 @@
   ...
 }: let
   cfg = config.option.gtk;
-  inherit (lib) mkEnableOption mkIf mkOption types;
+  inherit (lib) mkEnableOption mkIf mkOption types mkMerge mkForce;
 in {
   options.option.gtk = {
     enable = mkEnableOption "Enable Gtk Theme";
@@ -14,22 +14,39 @@ in {
       type = types.package;
       default = pkgs.nordic;
     };
+    thname = mkOption {
+      type = with types; nullOr str;
+      default = null;
+    };
     icon = mkOption {
       type = types.package;
       default = pkgs.candy-icons;
     };
   };
-  config = mkIf cfg.enable {
-    gtk = {
-      enable = true;
-      theme = {
-        package = cfg.theme;
-        name = "${cfg.theme.pname}";
+  config = mkMerge [
+    (mkIf cfg.enable {
+      gtk = {
+        enable = true;
+        theme = {
+          package = cfg.theme;
+          name =
+            if builtins.hasAttr "srcs" cfg.theme
+            then let
+              sorc = builtins.elemAt cfg.theme.srcs 0;
+            in
+              sorc.name
+            else if builtins.hasAttr "src" cfg.theme
+            then cfg.theme.pname
+            else {};
+        };
+        iconTheme = {
+          package = cfg.icon;
+          name = "${cfg.icon.pname}";
+        };
       };
-      iconTheme = {
-        package = cfg.icon;
-        name = "${cfg.icon.pname}";
-      };
-    };
-  };
+    })
+    (mkIf (cfg.thname != null) {
+      gtk.theme.name = mkForce cfg.thname;
+    })
+  ];
 }
